7fbed19cce5b2cd2e7ac60769671fb5dc8371a83
Matthew Johnson (HU) <matjoh@microsoft.com>
2019-12-16 19:21:41 +0000

    Working solution

--- python/day16.py
+++ python/day16.py
@@ -7,44 +7,74 @@ import pytest
 
 from common import asset
 
-
-def _compute_output(signal, index):
-    output = 0
+def _compute_output_strided(input_signal, index):
     num_repeats = index + 1
+
+    output = 0
+    end = len(input_signal)
+    pos_start = num_repeats - 1
+    neg_start = pos_start + 2*num_repeats
+    for i in range(num_repeats):
+        pos = np.sum(input_signal[pos_start+i:end:num_repeats*4])
+        neg = np.sum(input_signal[neg_start+i:end:num_repeats*4])
+        output += pos - neg
+    
+    return abs(output)%10
+
+def _compute_output_block(input_signal, index):
     pattern = [0, 1, 0, -1]
     current = 0
-    count = num_repeats
-    for i, val in enumerate(signal[index:]):
-        if count == num_repeats:
-            current = (current+1)%4
-            count = 0
-        
-        mult = pattern[current]
-        output += val*mult
-        count += 1
-    
-    return abs(output) % 10
-    
-
-def _fft_step(input_signal, output_signal):
+    num_repeats = index + 1
+    output = 0
     signal_length = len(input_signal)
-    for i in range(signal_length):
-        output_signal[i] = _compute_output(input_signal, i)
+    count = num_repeats - 1
+    start = 0
+    while start < signal_length:
+        if pattern[current] != 0:
+            end = min(start + count, signal_length)
+            val = np.sum(input_signal[start:end])
+            output += pattern[current]*val
+        
+        start += count
+        current = (current + 1)%4
+        count = num_repeats
+    
+    return abs(output)%10
 
 
-def fft(signal, num_steps):
-    input_signal = [ord(char) - ord('0') for char in signal]
-    output_signal = [0]*len(signal)
-    start = time.time()
+def _compute_output_optim(input_signal, index):
+    assert index >= len(input_signal)//2
+    output = np.sum(input_signal[index:])
+    return abs(output)%10
+
+
+def _fft_step(input_signal, output_signal, offset=0):
+    signal_length = len(input_signal)
+    half_length = signal_length // 2
+    for i in range(offset, half_length):
+        output_signal[i] = _compute_output_block(input_signal, i)
+    
+    output = 0
+    start = signal_length - 1
+    end = max(offset, half_length) - 1
+    for i in range(start, end, -1):
+        output += input_signal[i]
+        output_signal[i] = abs(output)%10
+
+
+def fft(signal, num_steps, use_offset=False):
+    offset = int(signal[:7]) if use_offset else 0
+    input_signal = np.frombuffer(signal.encode(), dtype=np.int8) - ord('0')
+    output_signal = np.zeros_like(input_signal)
     for _ in range(num_steps):
-        _fft_step(input_signal, output_signal)
+        _fft_step(input_signal, output_signal, offset)
         input_signal, output_signal = output_signal, input_signal
         sys.stdout.write('.')
         sys.stdout.flush()
 
-    sys.stdout.write('\n')    
-    print("Time per step:", (time.time() - start)/100)
-    return input_signal
+    sys.stdout.write('\n')
+
+    return "".join([str(val) for val in input_signal[offset:offset+8]])
 
 
 @pytest.mark.parametrize("signal, expected, num_steps", [
@@ -55,7 +85,6 @@ def fft(signal, num_steps):
 ])
 def test_fft(signal, expected, num_steps):
     signal = fft(signal, num_steps)
-    signal = "".join([str(val) for val in signal[:8]])
     assert signal == expected
 
 
@@ -78,10 +107,7 @@ def _main():
     signal = "".join([str(val) for val in signal[:8]])
     print("Part 1:", signal)
 
-    original_signal = "03036732577212944063491565474664"
-    signal = fft(original_signal*10000, 100)
-    offset = int(original_signal[:7])
-    signal = "".join([str(val) for val in signal[offset:offset+8]])
+    signal = fft(original_signal*10000, 100, True)
     print("Part 2:", signal)
 
 
