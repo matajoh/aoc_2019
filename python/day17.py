0252533b86749ba69ba91f4824a0d16875253e79
Matthew Johnson (MSRC) <matjoh@microsoft.com>
2019-12-17 16:05:39 +0000

    Solution to day 17

--- python/day17.py
+++ python/day17.py
@@ -1,3 +1,5 @@
+""" Solution to day 17 """
+
 import sys
 from collections import namedtuple
 from typing import Set
@@ -19,23 +21,25 @@ class Vector(namedtuple("Vector", ["x", "y"])):
     def find_neighbors(self) -> Set["Vector"]:
         """ The cardinal neighbors of this vector """
         return {self + move for move in DIRECTIONS}
-    
+
     def is_intersection(self, scaffolds):
+        """ Tests if this scaffold is an intersection """
         neighbors = self.find_neighbors()
         return len(neighbors & scaffolds) == 4
 
     @property
     def alignment_parameter(self):
-        """ L1 length of the vector """
+        """ Used for verification """
         return self.x*self.y
-    
 
     @property
     def length(self):
+        """ The L1 length of the vector """
         return abs(self.x) + abs(self.y)
-    
+
     @property
     def direction(self):
+        """ The direction of the vector """
         assert self.x == 0 or self.y == 0
         norm = Vector(-1 if self.x < 0 else (1 if self.x > 0 else 0),
                       -1 if self.y < 0 else (1 if self.y > 0 else 0))
@@ -53,37 +57,40 @@ ROBOT = "^>v<"
 
 
 class Robot:
+    """ Represents the vacuum robot """
+
     def __init__(self, position, direction):
         self.position = position
         self.direction = direction
         self.commands = []
 
     def valid_move(self, scaffolds):
-        left = (self.direction - 1)%4
-        right = (self.direction + 1)%4
+        """ Returns the valid move from the current location """
+        left = (self.direction - 1) % 4
+        right = (self.direction + 1) % 4
         left = self.position + DIRECTIONS[left]
         right = self.position + DIRECTIONS[right]
         if left in scaffolds:
             return left - self.position
-        
+
         if right in scaffolds:
             return right - self.position
-        
+
         return None
-    
+
     def add_segment(self, vector):
+        """ Adds a segment to the robots path """
         direction = vector.direction
-        if direction == (self.direction - 1)%4:
+        if direction == (self.direction - 1) % 4:
             self.commands.append("L")
-        elif direction == (self.direction + 1)%4:
+        elif direction == (self.direction + 1) % 4:
             self.commands.append("R")
         else:
             raise AssertionError("Invalid direction")
-            
+
         self.direction = direction
         self.position += vector
         self.commands.append(str(vector.length))
-
 
 
 def _parse_scaffolds(text):
@@ -94,7 +101,7 @@ def _parse_scaffolds(text):
         for x, char in enumerate(line):
             if char != '.':
                 scaffolds.add(Vector(x, y))
-            
+
             if char in ROBOT:
                 robot = Robot(Vector(x, y), ROBOT.index(char))
 
@@ -120,7 +127,7 @@ def _follow_scaffolds(scaffolds, robot):
     visited = set([start])
     while len(visited) < num_scaffolds:
         loc = end + direction
-        if end in scaffolds:
+        if loc in scaffolds:
             visited.add(loc)
             end = loc
         else:
@@ -134,6 +141,108 @@ def _follow_scaffolds(scaffolds, robot):
         robot.add_segment(end - start)
 
 
+def _to_routine_list(text):
+    parts = text.split(',')
+    routine = []
+    for part in parts:
+        if part in ('R', 'L'):
+            routine.append(part)
+        else:
+            routine.extend(['1']*int(part))
+
+    return " ".join(routine)
+
+
+def _to_routine_text(routine):
+    parts = routine.split()
+    text = []
+    count = 0
+    for part in parts:
+        if part in ('R', 'L', 'A', 'B', 'C'):
+            if count:
+                text.append(str(count))
+                count = 0
+
+            text.append(part)
+        else:
+            count += 1
+
+    if count:
+        text.append(str(count))
+
+    return ",".join(text)
+
+
+def _replace_subroutine(routine, sub_a, sub_b=None, sub_c=None):
+    routine = _to_routine_list(routine)
+    routine = routine.replace(_to_routine_list(sub_a), "A")
+    if sub_b:
+        routine = routine.replace(_to_routine_list(sub_b), "B")
+
+    if sub_c:
+        routine = routine.replace(_to_routine_list(sub_c), "C")
+
+    return _to_routine_text(routine)
+
+
+def _principal_period(s):
+    i = (s+s).find(s, 1, -1)
+    return None if i == -1 else s[:i]
+
+
+def _find_subroutines(routine):
+    routine = _to_routine_list(routine) + " "
+    parts = routine.split()
+    max_length = len(parts) // 2
+    for i in range(4, max_length):
+        sub_a = " ".join(parts[:i]) + " "
+        if routine.find(sub_a) == routine.rfind(sub_a):
+            break
+
+        routine_a = routine.replace(sub_a, "")
+        for j in range(4, max_length):
+            sub_b = " ".join(parts[-j:]) + " "
+            if routine.find(sub_b) == routine.rfind(sub_b):
+                break
+
+            routine_ab = routine_a.replace(sub_b, "")
+            sub_c = _principal_period(routine_ab)
+            if sub_c:
+                routine_abc = routine.replace(sub_a, "A ")
+                routine_abc = routine_abc.replace(sub_b, "B ")
+                routine_abc = routine_abc.replace(sub_c, "C ")
+                routine_abc = routine_abc.strip().replace(' ', ',') + '\n'
+                if len(routine_abc) <= 20:
+                    sub_a = _to_routine_text(sub_a) + '\n'
+                    sub_b = _to_routine_text(sub_b) + '\n'
+                    sub_c = _to_routine_text(sub_c) + '\n'
+                    return routine_abc, sub_a, sub_b, sub_c
+
+    return None
+
+
+def _run_routine(program, routine, sub_a, sub_b, sub_c):
+    program[0] = 2
+    computer = Computer(program)
+
+    while not computer.needs_input:
+        computer.step()
+
+    for char in routine + sub_a + sub_b + sub_c + "n\n":
+        computer.write(ord(char))
+
+    val = None
+    while not computer.is_halted:
+        while not computer.num_outputs:
+            computer.step()
+
+        val = computer.read()
+        if val < 255:
+            sys.stdout.write(chr(val))
+
+    return val
+
+
 TEST = """..#..........
 ..#..........
 #######...###
@@ -145,6 +254,7 @@ TEST = """..#..........
 
 
 def test_sum_alignment_params():
+    """ Test """
     scaffolds, _ = _parse_scaffolds(TEST)
     assert _sum_of_alignment_parameters(scaffolds) == 76
 
@@ -157,7 +267,7 @@ def _ascii(program):
     while not computer.is_halted:
         while not computer.num_outputs:
             computer.step()
-        
+
         tile = chr(computer.read())
         if last == tile == '\n':
             break
@@ -165,21 +275,24 @@ def _ascii(program):
         last = tile
         output.write(tile)
         sys.stdout.write(tile)
-    
+
     return output.getvalue()
+
 
 def _main():
     with open(asset("day17.txt")) as file:
         program = [int(part) for part in file.read().split(',')]
-    
+
     text = _ascii(program)
 
     scaffolds, robot = _parse_scaffolds(text)
     print("Part 1:", _sum_of_alignment_parameters(scaffolds))
 
     _follow_scaffolds(scaffolds, robot)
-    print(robot.commands)
+    routine = ",".join(robot.commands)
 
+    routine, sub_a, sub_b, sub_c = _find_subroutines(routine)
+    print("Part 2:", _run_routine(program, routine, sub_a, sub_b, sub_c))
 
 
 if __name__ == "__main__":
